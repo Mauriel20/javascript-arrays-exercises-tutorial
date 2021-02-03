b1c76418d6cf61d9f1cbcd8c8e4c8b83dcc9d390
Mauriel <josueazofeifajosue@gmail.com>
2021-02-03 23:10:10 +0000

    My-work

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,36 @@
 // Code goes here
-//let number = Math.floor(Math.random()*2);
-const matrix = new Array(5).fill(0).map(() => new Array(5).fill(0));
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// function evalueateMatrix(number) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr[i].length; j++) {
+//             if (arr[i][j] == number) {
+//                 console.log("[" +i+","+j+"]");
+//             }
 
+//         }
+
+//     }
+
+// }
+
+const matrixBuilder = (number) => {
+    let matriz = [];
+
+    for (let i = 0; i < number; i++) {
+
+        let fila=[]
+        for (let j = 0; j < number; j++) {
+            let num= 1//Math.round(Math.random()*1);
+            fila.push(num);
+        }
+        matriz.push(fila);
+    }
+    return matriz;
+
+}
 // do not change anything from this line down
-console.log(matrix)
+console.log(matrixBuilder(5));
--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -3,4 +3,24 @@ let parking_state = [
   [0,0,0],
   [1,1,2]
 ]
-console.log(getParkingLotState(parking_state))
+const getParkingLotState = (parking_state)=>{
+   let state = {
+        totalSlots:0,
+        availableSlots: 0,
+       occupiedSlots:0
+   }
+   parking_state.forEach(filas => {
+        filas.forEach(columnas =>{
+            if(columnas == 1){
+                state.occupiedSlots++;
+                state.totalSlots++;
+            }else if(columnas == 2){
+                
+                state.availableSlots++;
+                state.totalSlots++;
+            }
+        });
+    }); 
+    return state;  
+}
+console.log(getParkingLotState(parking_state));
